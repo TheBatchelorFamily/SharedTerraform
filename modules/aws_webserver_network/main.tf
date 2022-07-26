36ca7c10779fc8db76f55b0a8b2c823ea539443a
christsreturn <christhebatchelor@gmail.com>
2022-07-26 13:42:09 -0500

    adding instance key for counted resources

--- modules/aws_webserver_network/main.tf
+++ modules/aws_webserver_network/main.tf
@@ -59,19 +59,19 @@ resource "aws_eip" "webserver" {
 resource "aws_route53_record" "www" {
   count = var.r53Enabled ? 0:1
   depends_on = [ aws_eip.webserver ]
-  name    = "www.${data.aws_route53_zone.website.name}"
+  name    = "www.${data.aws_route53_zone.website[*].name}"
   records = [ aws_eip.webserver.public_ip ]
   type    = "A"
   ttl     = "300"
-  zone_id = data.aws_route53_zone.website.zone_id
+  zone_id = data.aws_route53_zone.website[*].zone_id
 }
 
 resource "aws_route53_record" "no_www" {
   count = var.r53Enabled ? 1:0
   depends_on = [ aws_eip.webserver ]
-  name    = data.aws_route53_zone.website.name
+  name    = data.aws_route53_zone.website[*].name
   records = [ aws_eip.webserver.public_ip ]
   type    = "A"
   ttl     = "300"
-  zone_id = data.aws_route53_zone.website.zone_id
+  zone_id = data.aws_route53_zone.website[*].zone_id
 }
