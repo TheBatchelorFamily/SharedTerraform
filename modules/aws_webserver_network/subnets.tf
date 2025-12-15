adbc904448ae81d16f09a9d42a8c78715ee60d05
Chris Batchelor <christhebatchelor@gmail.com>
2025-12-15 10:59:27 -0600

    Fix subnets

--- modules/aws_webserver_network/subnets.tf
+++ modules/aws_webserver_network/subnets.tf
@@ -1,3 +1,8 @@
+# Get VPC details to use its CIDR block
+data "aws_vpc" "webserver" {
+  id = var.vpc
+}
+
 # Get available AZs in the region
 data "aws_availability_zones" "available" {
   state = "available"
@@ -5,9 +10,9 @@ data "aws_availability_zones" "available" {
 
 # Create subnets in multiple AZs for high availability and capacity flexibility
 resource "aws_subnet" "webserver" {
-  count             = min(length(data.aws_availability_zones.available.names), 3)
+  count             = length(data.aws_availability_zones.available.names)
   vpc_id            = var.vpc
-  cidr_block        = "10.0.${count.index + 1}.0/24"
+  cidr_block        = cidrsubnet(data.aws_vpc.webserver.cidr_block, 8, 10 + count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   map_public_ip_on_launch = true
