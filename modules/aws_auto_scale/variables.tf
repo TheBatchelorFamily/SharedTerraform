a80906860bae710d1c81d670b9254c044449e897
Chris Batchelor <christhebatchelor@gmail.com>
2026-04-16 16:39:47 -0500

    Add Variable types

--- modules/aws_auto_scale/variables.tf
+++ modules/aws_auto_scale/variables.tf
@@ -1,38 +1,46 @@
 variable "iamRoleName" {
+  type        = string
   default     = "assign-eip"
   description = "Name for iam policy"
 }
 
 variable "iType" {
+  type        = string
   default     = "t3.micro"
   description = "ec2 instance size"
 }
 
 variable "keyname" {
+  type        = string
   default     = "webserver-key"
   description = "The name of the ssh key to create"
 }
 
 variable "publicIP" {
+  type        = bool
   default     = false
   description = "Determines if a public IP is assigned to the instances built from the auto scale template"
 }
 
 variable "sshPub" {
+  type        = string
   default     = ""
   description = "The public key value for the ssh key being created"
 }
 
 variable "securityGroup" {
+  type        = list(string)
   description = "The security group ID of the existing security group to assign"
 }
 
 variable "userData" {
+  type        = string
   default     = ""
   description = "Script for the ec2 instance to run at launch"
 }
 
 variable "tags" {
+  type        = map(string)
   default     = { Name = "Webserver" }
   description = "Tags to apply to created resources"
 }
--- modules/aws_webserver_network/variables.tf
+++ modules/aws_webserver_network/variables.tf
@@ -1,34 +1,41 @@
 variable "dnsName" {
+  type        = string
   default     = "itsmeganificent.com"
   description = "The existing Route53 DNS name to use."
 }
 
 variable "r53Enabled" {
+  type        = bool
   default     = true
   description = "Enables or disables route53 components so that this module can be run without modifying dns"
 }
 
 variable "region" {
+  type        = string
   default     = "us-east-1"
   description = "The aws region to deploy to"
 }
 
 variable "secgroupname" {
+  type        = string
   default     = "Webserver-Sec-Group"
   description = "The name of the security group to create"
 }
 
 variable "sshIP" {
+  type        = list(string)
   default     = ["136.32.254.0/24"]
   description = "The subnet range that allows SSH connectivity"
 }
 
 variable "tags" {
+  type        = map(string)
   default     = { Name = "Webserver" }
   description = "Tags to apply to created resources"
 }
 
 variable "vpc" {
+  type        = string
   default     = "vpc-00a0663f397146f3d"
   description = "An existing VPC ID."
 }
